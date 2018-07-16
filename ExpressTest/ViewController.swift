c5ae666850702f2d51e49de70890dc522bcd84b6
Renxiao Mo <mrenxiao@gmail.com>
2018-07-16 11:57:59 +0200

    display words

--- ExpressTest/ViewController.swift
+++ ExpressTest/ViewController.swift
@@ -12,6 +12,12 @@ class ViewController: UIViewController {
 
   let numberOfColumns = 9
   let numberOfRows = 11
+
+  let words = [
+    (string: "Animal", x: 2, y: 0),
+    (string: "Busy", x:4, y: 5),
+    (string: "Computer", x: 1, y: 9)
+    ]
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -33,7 +39,25 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, 
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LetterCellIdentifier, for: indexPath) as! LetterCell
     
+    for word in words {
+      let row = indexPath.item / 9
+      let column = indexPath.item % 9
+      
+      if (row == word.y &&
+        column >= word.x &&
+        column < word.string.count + word.x) {
+        
+        let letterIndex = word.string.index(word.string.startIndex, offsetBy: column - word.x)
+        let letterString = word.string[letterIndex]
+        cell.letterLabel.text = String(letterString)
+      }
+    }
+    
     return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
   }
   
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
