ca4af05a00f23a0a5cd84ec760dbeb1a4b8660f4
Renxiao Mo <mrenxiao@gmail.com>
2018-07-16 10:43:04 +0200

    setup collectionView

---
+++ ExpressTest/LetterCell.swift
@@ -0,0 +1,17 @@
+//
+//  LetterCell.swift
+//  ExpressTest
+//
+//  Created by Calvin Mo on 16/07/2018.
+//  Copyright © 2018 Calvin Mo. All rights reserved.
+//
+
+import UIKit
+
+let LetterCellIdentifier = "LetterCell"
+
+class LetterCell: UICollectionViewCell {
+    
+  @IBOutlet weak var letterLabel: UILabel!
+  
+}
--- ExpressTest/ViewController.swift
+++ ExpressTest/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  let numberOfColumns = 9
+  let numberOfRows = 11
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,26 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return numberOfColumns * numberOfRows
+  }
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LetterCellIdentifier, for: indexPath) as! LetterCell
+    
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    
+    let screenRect = UIScreen.main.bounds
+    let screenWidth = screenRect.size.width
+    let cellWidth = screenWidth/CGFloat(numberOfColumns) - 1
+    
+    return CGSize(width: cellWidth, height: cellWidth)
+  }
+}
